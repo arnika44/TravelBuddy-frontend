f2fa4fb88b9ff9fa3a63b3bbc240265716e68775
arnika44 <arnika2135@gmail.com>
2026-07-24 11:25:57 +0530

    Forgot password email update

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -7,83 +7,16 @@ const API = "https://travelbuddy-backend-4w7o.onrender.com";
 export default function ForgotPassword() {
 
   const navigate = useNavigate();
-
-  const [phone, setPhone] = useState("");
-  const [otp, setOtp] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [email, setEmail] = useState("");
 
-  const [otpSent, setOtpSent] = useState(false);
-  const [otpVerified, setOtpVerified] = useState(false);
-
-  const sendOtp = async () => {
-
-  if (!phone) {
-
-    alert("Enter phone number");
-
-    return;
-
-  }
-
-  try {
-
-    const res = await axios.post(`${API}/send-otp`, {
-      phone
-    });
-
-    alert(res.data.message);
-
-    setOtpSent(true);
-
-  } catch (err) {
-
-    alert("Failed to send OTP");
-
-  }
-
-};
-
-const verifyOtp = async () => {
-
-  if (!otp) {
-
-    alert("Enter OTP");
-
-    return;
-
-  }
-
-  try {
-
-    const res = await axios.post(
-      `${API}/forgot-password-verify-otp`,
-      {
-        phone,
-        otp
-      }
-    );
-
-    if (res.data.success) {
-
-      alert(res.data.message);
-
-      setOtpVerified(true);
-
-    } else {
-
-      alert(res.data.message);
-
-    }
-
-  } catch (err) {
-
-    alert("OTP verification failed");
-
-  }
-
-};
 const resetPassword = async () => {
+
+  if (!email) {
+  alert("Enter Email");
+  return;
+}
 
   if (!newPassword || !confirmPassword) {
     alert("Fill all fields");
@@ -98,7 +31,7 @@ const resetPassword = async () => {
   try {
 
     const res = await axios.post(`${API}/reset-password`, {
-      phone,
+      email,
       newPassword
     });
 
@@ -128,62 +61,15 @@ const resetPassword = async () => {
       <h2>Forgot Password</h2>
 
       <input
-        placeholder="Phone Number"
-        value={phone}
-        onChange={(e) => setPhone(e.target.value)}
-        style={{
-          width: "100%",
-          padding: "10px",
-          marginBottom: "10px"
-        }}
-      />
-
-      {!otpSent && (
-
-        <button
-          onClick={sendOtp}
-          style={{
-            width: "100%",
-            padding: "10px"
-          }}
-        >
-          Send OTP
-        </button>
-
-      )}
-
-      {otpSent && !otpVerified && (
-
-        <>
-
-          <input
-            placeholder="Enter OTP"
-            value={otp}
-            onChange={(e) => setOtp(e.target.value)}
-            style={{
-              width: "100%",
-              padding: "10px",
-              marginTop: "10px",
-              marginBottom: "10px"
-            }}
-          />
-
-          <button
-            onClick={verifyOtp}
-            style={{
-              width: "100%",
-              padding: "10px"
-            }}
-          >
-            Verify OTP
-          </button>
-
-        </>
-
-      )}
-
-      {otpVerified && (
-
+  placeholder="Email"
+  value={email}
+  onChange={(e)=>setEmail(e.target.value)}
+  style={{
+    width:"100%",
+    padding:"10px",
+    marginBottom:"10px"
+  }}
+/>
         <>
 
           <input
@@ -226,8 +112,6 @@ const resetPassword = async () => {
           </button>
 
         </>
-
-      )}
 
     </div>
 
